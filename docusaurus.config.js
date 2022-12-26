0f948ac1125afbbf456da913fa4bdc87ce046f19
Laure Kamalandua <12623827+laurekamalandua@users.noreply.github.com>
2022-12-26 21:58:13 +0100

    updated site config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         logo: {
           alt: 'Quantaleap Logo',
